50fddb486168a11955d380f1822cf8fc07c19090
yoon <fana9088@naver.com>
2025-04-14 22:57:58 +0900

    refactor : 이미지가 열리지않고 다운로드되던 현상 수정

--- src/main/java/animation/S3/S3Service.java
+++ src/main/java/animation/S3/S3Service.java
@@ -2,6 +2,7 @@ package animation.S3;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class S3Service {
@@ -22,15 +24,27 @@ public class S3Service {
     private String bucketName;
 
     public String uploadFile(MultipartFile multipartFile) throws IOException {
-        File file = multiPartFileToFile(multipartFile);
         String fileName = System.currentTimeMillis() + "_" + multipartFile.getOriginalFilename();
 
-        // 객체를 공개로 설정
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file)
-                .withCannedAcl(CannedAccessControlList.PublicRead);
-        amazonS3.putObject(putObjectRequest);
-        file.delete();
+        // 메타데이터 설정(이미지가 다운로드 되지않도록 inline으로)
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(multipartFile.getContentType());
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setHeader("Content-Disposition", "inline");
 
+        // 스트림으로 직접 업로드
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    bucketName,
+                    fileName,
+                    inputStream,
+                    metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead);
+
+            amazonS3.putObject(putObjectRequest);
+        }
+
+        // 이미지 URL 생성
         return amazonS3.getUrl(bucketName, fileName).toString();
     }
 
