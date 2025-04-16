5efcb3389c3a855f672d2c1c8b7addf5f118c0d0
TJOEUNIS <HwaEVE@gmail.com>
2025-04-16 10:50:36 +0900

    refactor : anime 생성 post로 변경

--- src/main/java/animation/anime/AnimeController.java
+++ src/main/java/animation/anime/AnimeController.java
@@ -4,6 +4,7 @@ import animation.anime.dto.AnimeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class AnimeController {
 
     private final AnimeService animeService;
 
-    @GetMapping("/api/anime/{malId}")
+    @PostMapping("/anime/{malId}")
     public AnimeResponse importAnime(@PathVariable Long malId) {
          return animeService.importAnimeById(malId);
     }
